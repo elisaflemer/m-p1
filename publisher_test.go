057ec99d4b0bf9bae9de3e1c5200690c4024ebf6
Elisa Flemer <elisaflemer@gmail.com>
2024-02-25 16:52:40 -0300

    remoe err

--- publisher_test.go
+++ publisher_test.go
@@ -55,7 +55,7 @@ func getConnectionType(t *testing.T) MQTTConnector {
 
 func TestConnectMQTT(t *testing.T) {
 
-	err := godotenv.Load(".env")
+	godotenv.Load(".env")
 	t.Log(os.Getenv("HIVEMQ_USERNAME"), os.Getenv("HIVEMQ_PASSWORD"))
 	connector := getConnectionType(t)
 	client := connector.Connect("publisher")
